7ed6465de1e5727278933737be04395af21b76d4
walkerchi <walkerchi@foxmail.com>
2024-09-05 14:27:45 +0200

    update

--- task1_example1.py
+++ task1_example1.py
@@ -16,8 +16,9 @@ legend_loc = "upper left" # location of the legendS
 dpi = 200 # DPI for the saved figure
 fps = 10 # FPS for the saved animation
 figsize = (10, 5) # Size of the figure
+only_static = True
 ####################################################
-
+import os
 import numpy as np
 import pandas as pd
 import matplotlib
@@ -73,80 +74,114 @@ X_test = np.linspace(0, 1, len(dates)).reshape(-1, 1)
 kernel = GPy.kern.RBF(input_dim=1, variance=var, lengthscale=l)
 # gp_model = GPy.models.GPRegression(x, y, kernel)
 
-# Step 3: Set up the figure, axis, and plot element to animate
-fig, ax = plt.subplots(figsize=figsize)
-ax.set_xlim(dates[0], dates[-1])
-ax.set_ylim(ylims[0], ylims[1]*n_teams)
-ax.set_xlabel("Date")
-ax.set_ylabel("Amount in CHF(Million)")
 
-line, = ax.plot([], [], lw=2, label="Mean Prediction", color=color)
-fill_between = None  # For confidence intervals
-observations = []
-for i in range(n_teams):
-    observations.append(ax.scatter([], [], s=scatter_size, c=scatter_color[i], marker=scatter_marker[i], label=f"Team {i} Observations"))
-observations.append(ax.scatter([], [], s=scatter_size, c=scatter_color[n_teams], marker=scatter_marker[n_teams], label=f"Observations Sum"))
+if not only_static:
+    # Step 3: Set up the figure, axis, and plot element to animate
+    fig, ax = plt.subplots(figsize=figsize)
+    ax.set_xlim(dates[0], dates[-1])
+    ax.set_ylim(ylims[0], ylims[1]*n_teams)
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Amount in CHF(Million)")
 
-# Set date format on x-axis
-date_format = DateFormatter("%Y-%m")
-ax.xaxis.set_major_formatter(date_format)
-months = MonthLocator(bymonth=(1, 6))
-ax.xaxis.set_major_locator(months)
+    line, = ax.plot([], [], lw=2, label="Mean Prediction", color=color)
+    fill_between = None  # For confidence intervals
+    observations = []
+    for i in range(n_teams):
+        observations.append(ax.scatter([], [], s=scatter_size, c=scatter_color[i], marker=scatter_marker[i], label=f"Team {i} Observations"))
+    observations.append(ax.scatter([], [], s=scatter_size, c=scatter_color[n_teams], marker=scatter_marker[n_teams], label=f"Observations Sum"))
 
-ax.legend(loc=legend_loc)
+    # Set date format on x-axis
+    date_format = DateFormatter("%Y-%m")
+    ax.xaxis.set_major_formatter(date_format)
+    months = MonthLocator(bymonth=(1, 6))
+    ax.xaxis.set_major_locator(months)
 
-# Step 4: Initialize the plot elements
-def init():
-    line.set_data([], [])
-    for observation in observations:
-        observation.set_offsets(np.c_[[], []])
-    return [line, *observations]
+    ax.legend(loc=legend_loc)
 
-pbar = tqdm(total=len(x)-1, desc="Animating", colour="green")
-
-# Step 5: Define the update function for the animation
-def update(frame):
-    global x, y, fill_between, color
-
-    if frame == 0:
+    # Step 4: Initialize the plot elements
+    def init():
+        line.set_data([], [])
+        for observation in observations:
+            observation.set_offsets(np.c_[[], []])
         return [line, *observations]
 
-    # Add a new point from the true function (simulate more observations over time)
-    new_x = x[:frame].repeat(n_teams, 1)
-    new_y = y[:frame]
+    pbar = tqdm(total=len(x)-1, desc="Animating", colour="green")
 
-    # Update the GP model with new data
-    gp_model = GPy.models.SparseGPRegression(new_x, new_y, kernel, num_inducing=32)
-    gp_model.optimize()
+    # Step 5: Define the update function for the animation
+    def update(frame):
+        global x, y, fill_between, color
 
-    # Predict using the GP
-    Y_pred, Y_var = gp_model.predict(X_test)
-    Y_pred = Y_pred.sum(-1)
-    Y_var  = Y_var[:,-1]
+        if frame == 0:
+            return [line, *observations]
 
-    # Update plot data
-    line.set_data(dates, Y_pred)
+        # Add a new point from the true function (simulate more observations over time)
+        new_x = x[:frame].repeat(n_teams, 1)
+        new_y = y[:frame]
 
-    # Remove old confidence interval if it exists
-    if fill_between:
-        fill_between.remove()
+        # Update the GP model with new data
+        gp_model = GPy.models.SparseGPRegression(new_x, new_y, kernel, num_inducing=32)
+        gp_model.optimize()
 
-    # Update the confidence intervals (mean +/- 1.96 * stddev)
-    fill_between = ax.fill_between(dates, 
-                                (Y_pred - 1.96 * np.sqrt(Y_var)).flatten(), 
-                                (Y_pred + 1.96 * np.sqrt(Y_var)).flatten(), 
-                                color=color, alpha=0.3)
+        # Predict using the GP
+        Y_pred, Y_var = gp_model.predict(X_test)
+        Y_pred = Y_pred.sum(-1)
+        Y_var  = Y_var[:,-1]
 
-    # Update observed points
-    for i in range(n_teams):
-        observations[i].set_offsets(np.c_[dates[:frame], new_y[:,i]])
-    observations[n_teams].set_offsets(np.c_[dates[:frame], new_y.sum(-1)])
+        # Update plot data
+        line.set_data(dates, Y_pred)
 
-    pbar.update(1)
+        # Remove old confidence interval if it exists
+        if fill_between:
+            fill_between.remove()
 
-    return [line, *observations, fill_between]
+        # Update the confidence intervals (mean +/- 1.96 * stddev)
+        fill_between = ax.fill_between(dates, 
+                                    (Y_pred - 1.96 * np.sqrt(Y_var)).flatten(), 
+                                    (Y_pred + 1.96 * np.sqrt(Y_var)).flatten(), 
+                                    color=color, alpha=0.3)
 
-# Step 6: Animate the plot using FuncAnimation
-ani = FuncAnimation(fig, update, frames=len(x), init_func=init, blit=True, interval=200)
+        # Update observed points
+        for i in range(n_teams):
+            observations[i].set_offsets(np.c_[dates[:frame], new_y[:,i]])
+        observations[n_teams].set_offsets(np.c_[dates[:frame], new_y.sum(-1)])
 
-ani.save("outputs/gp_animation.mp4", fps=fps, dpi=dpi)
+        pbar.update(1)
+
+        return [line, *observations, fill_between]
+
+    # Step 6: Animate the plot using FuncAnimation
+    ani = FuncAnimation(fig, update, frames=len(x), init_func=init, blit=True, interval=200)
+    os.makedirs("outputs",exist_ok=True)
+    ani.save("outputs/gp_animation.mp4", fps=fps, dpi=dpi)
+
+fig, ax = plt.subplots(figsize=(12,6))
+ax.set_xlim(dates[0], dates[-1])
+ax.set_ylim(ylims[0], ylims[1]*n_teams)
+ax.set_xlabel("Date", fontdict={'fontsize':14})
+ax.set_ylabel("Amount in CHF(Million)", fontdict={'fontsize':14})
+
+new_x = x.repeat(n_teams, 1)
+new_y = y
+
+gp_model = GPy.models.SparseGPRegression(new_x, new_y, kernel, num_inducing=32)
+gp_model.optimize()
+
+Y_pred, Y_var = gp_model.predict(X_test)
+Y_pred = Y_pred.sum(-1)
+Y_var  = Y_var[:,-1]
+
+ax.plot(dates, Y_pred, color=color, label="Mean")
+
+
+fill_between = ax.fill_between(dates, 
+                            (Y_pred - 1.96 * np.sqrt(Y_var)).flatten(), 
+                            (Y_pred + 1.96 * np.sqrt(Y_var)).flatten(), 
+                            color=color, alpha=0.3)
+
+# Update observed points
+for i in range(n_teams):
+    ax.scatter(dates, new_y[:,i],s=scatter_size, c=scatter_color[i], marker=scatter_marker[i], label=f"Team {i} Observations")
+ax.scatter(dates, new_y.sum(-1), s=scatter_size, c=scatter_color[n_teams], marker=scatter_marker[n_teams], label=f"Observations Sum")
+
+ax.legend()
+fig.savefig("outputs/gp_static.png")
